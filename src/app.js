a6db9609bae6bb72cedd39f0bcfb115a89101c18
aledrb <alejandradrb@gmail.com>
2021-06-06 18:02:26 +0000

    carta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+
+  let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let pinta = ["&hearts;", "&clubs;", "&spades;", "&diams;"];
+
+  function randomnumber(number) {
+    var random = Math.floor(Math.random() * number.length);
+    return number[random];
+  }
+
+  function show() {
+    const cara = randomnumber(pinta);
+    if (cara === "&hearts;" || cara === "&diams;") {
+      let x, i;
+      x = document.querySelectorAll(".naipes");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.color = "red";
+      }
+    }
+    document.getElementsByClassName("naipes")[0].innerHTML = cara;
+    document.getElementsByClassName("naipes")[1].innerHTML = cara;
+    document.getElementById("cartanumero").innerHTML = randomnumber(numeros);
+  }
+
+  show();
 };
